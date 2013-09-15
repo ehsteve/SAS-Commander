6fead2ce2cbf5f6bab93b3d5e6f94e5153118dac
Steven Christe <steven.d.christe@nasa.gov>
2013-09-14 23:42:03 -0600

    added autosaving to file

--- SAS-Commander/ConsoleWindowController.m
+++ SAS-Commander/ConsoleWindowController.m
@@ -10,12 +10,17 @@
 
 @interface ConsoleWindowController ()
 - (void)copyToClipboard:(NSString*)str;
+- (NSString *)createDateTimeString;
+- (NSFileHandle *)openSaveFile;
+@property (nonatomic, strong) NSFileHandle *saveFile;
+
 @end
 
 @implementation ConsoleWindowController
 
 @synthesize lineNumber;
 @synthesize ConsoleTextView;
+@synthesize saveFile;
 
 - (id)init{
     return [super initWithWindowNibName:@"ConsoleWindowController"];
@@ -48,6 +53,18 @@
 
 - (IBAction)copy_button:(NSButton*)sender{
     [self copyToClipboard:[self.ConsoleTextView string]];
+}
+
+- (IBAction)savetofile_button:(NSButton *)sender {
+    if ([sender state] == NSOnState) {
+        if (self.saveFile == nil) {
+            self.saveFile = [[NSFileHandle alloc] init];
+            self.saveFile = [self openSaveFile];
+        }
+    } else {
+        [self.saveFile closeFile];
+        self.saveFile = nil;
+    }
 }
 
 -(void)copyToClipboard:(NSString*)str
@@ -84,6 +101,10 @@
                    range:selectedRange];
     [string endEditing];
     
+    if (self.saveFile) {
+        [self.saveFile writeData:[[string string] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
     [[self.ConsoleTextView textStorage] insertAttributedString:string atIndex:[[self.ConsoleTextView string] length]];
     
     // scroll to bottom
@@ -93,5 +114,32 @@
     lineNumber++;
 }
 
+- (NSFileHandle *)openSaveFile{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *filename = [NSString stringWithFormat:@"SAS-Commander-log_%@.txt", [self createDateTimeString]];
+    
+    NSString *filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:filename];
+    // open file to save data stream
+    NSFileHandle *theFileHandle = [NSFileHandle fileHandleForWritingAtPath: filePath ];
+    if (theFileHandle == nil) {
+        [[NSFileManager defaultManager] createFileAtPath:filePath contents:nil attributes:nil];
+        theFileHandle = [NSFileHandle fileHandleForWritingAtPath:filePath];
+    }
+    //say to handle where's the file fo write
+    [theFileHandle truncateFileAtOffset:[theFileHandle seekToEndOfFile]];
+    return theFileHandle;
+}
+
+- (NSString *)createDateTimeString{
+    // Create a time string with the format YYYYMMdd_HHmmss
+    // This can be used in file names (for example)
+    //
+    NSDate *currDate = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:@"YYYYMMdd_HHmmss"];
+    
+    NSString *dateString = [dateFormatter stringFromDate:currDate];
+    return dateString;
+}
 
 @end
